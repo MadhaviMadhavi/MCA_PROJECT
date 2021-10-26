3635a28e8941fa54a8d63c64cc8cc43b14e84571
Madhavi <createwithmadhavi@gmail.com>
2021-10-26 08:36:14 +0530

    MCA project

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div style={{ background: "lightgray" }}>
+      <h3>
+        <marquee>Just explore photos</marquee>
+        <marquee>By Madhavi Neerukattu</marquee>
+      </h3>
+    </div>
+  );
+}
+
+export default Header;
